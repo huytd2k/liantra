06d5b9587471ee9b54b52fdeca636b2a47571954
Mike <huy.td183768@sis.hust.edu.vn>
2020-04-01 19:46:00 +0700

    init validateJwt middleware

--- src/middleware/validateJwt.ts
+++ src/middleware/validateJwt.ts
@@ -1,1 +1,6 @@
-export class validateJwt {};
+import * as Express from 'express';
+
+
+export const validateJwt = async (req:Express.Request, res: Express.Response, next: Express.NextFunction ) => {
+    
+};
